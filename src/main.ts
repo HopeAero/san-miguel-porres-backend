b607a89c7613522347e173d4a8102bcdaad5528a
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2025-03-08 18:10:26 -0400

    agregar configuración de CORS, soporte para variables de entorno y mejoras en la estructura del proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS } from './common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import envConfig from './config/environment';
+import {
+  initializeTransactionalContext,
+  StorageDriver,
+} from 'typeorm-transactional';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const PORT = envConfig.PORT || 8000;
+
+  app.set('trust proxy', true);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.enableCors(CORS);
+
+  await app.listen(PORT);
+
+  console.log(`Servidor corriendo en el puerto: ${PORT}`);
 }
 bootstrap();
