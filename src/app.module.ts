85a82dbfed6e6faf13408b01d9d641cdb9cf4492
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2025-03-08 18:22:36 -0400

    agregar variables de entorno a .env.template y eliminar ConfigModule en app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from 'nestjs-config';
-import path from 'path';
 
 @Module({
-  imports: [ConfigModule.load(path.resolve(__dirname, 'config', '*.{ts,js}'))],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
