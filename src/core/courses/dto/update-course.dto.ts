4038d96e3754e32492e0f72bce6135307c08fa7a
feredev <feredev2@gmail.com>
2025-03-22 21:28:54 -0400

    chore: foixes tarea de gterencias

--- src/core/courses/dto/update-course.dto.ts
+++ src/core/courses/dto/update-course.dto.ts
@@ -1,5 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateCourseDto } from './create-course.dto';
 
-// Extends CreateCourseDto but makes all fields optional
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends CreateCourseDto {}
