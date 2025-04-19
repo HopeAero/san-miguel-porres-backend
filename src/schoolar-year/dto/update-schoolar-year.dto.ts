dcc823be225f46974c69f03335e9826a1c14f0ad
feredev <feredev2@gmail.com>
2025-04-18 22:12:19 -0400

    refactor: remove SchoolarYear module, controller, service, and DTOs to streamline codebase

--- src/schoolar-year/dto/update-schoolar-year.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSchoolarYearDto } from './create-schoolar-year.dto';
-
-export class UpdateSchoolarYearDto extends PartialType(CreateSchoolarYearDto) {}
