162e36dc27260abead212d631c453b4e15143d0c
feredev <feredev2@gmail.com>
2025-04-27 00:13:55 -0400

    feat: implement search functionality for representatives with filtering options and mandatory inclusion of specific IDs

--- src/core/people/student/dto/create-student.dto.ts
+++ src/core/people/student/dto/create-student.dto.ts
@@ -1,3 +1,14 @@
 import { CreatePersonDto } from '@/core/people/people/dto/create-person.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
-export class CreateStudentDto extends CreatePersonDto {}
+export class CreateStudentDto extends CreatePersonDto {
+  @ApiProperty({
+    description: 'ID del representante del estudiante',
+    example: 1,
+    required: true
+  })
+  @IsNumber()
+  @IsNotEmpty({ message: 'El representante es obligatorio' })
+  representativeId: number;
+}
--- src/core/people/student/student.module.ts
+++ src/core/people/student/student.module.ts
@@ -4,10 +4,11 @@ import { PeopleModule } from '../people/people.module';
 import { Student } from './entities/student.entity';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
+import { Representative } from '../representative/entities/representative.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Student]),
+    TypeOrmModule.forFeature([Student, Representative]),
     forwardRef(() => PeopleModule),
   ],
   controllers: [StudentController],
