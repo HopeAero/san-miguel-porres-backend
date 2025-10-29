fb6076f5a22230e53e2379a6c47075face421871
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2025-10-28 20:22:19 -0400

    refactor: remove debug console logs from RoleGuard, cleaning up the code for better readability

--- src/core/auth/guards/roles.guard.ts
+++ src/core/auth/guards/roles.guard.ts
@@ -31,8 +31,6 @@ export class RoleGuard implements CanActivate {
 
     const { user } = request;
 
-    console.log('user', user);
-
     if (!user) {
       return false;
     }
@@ -41,8 +39,6 @@ export class RoleGuard implements CanActivate {
       .createQueryBuilder(User, 'user')
       .where('user.email = :email', { email: user.email })
       .getOne();
-
-    console.log('toxxken', token);
 
     if (!token) {
       return false;
